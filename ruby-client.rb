13304b96452479d679791b3dfbf8828e83041826
Ryu Sato <ryu@weseek.co.jp>
2024-02-03 09:47:37 +0000

    add websocket server/client on ruby

---
+++ ruby-client.rb
@@ -0,0 +1,23 @@
+require 'websocket-eventmachine-client'
+
+EM.run do
+
+  ws = WebSocket::EventMachine::Client.connect(:uri => 'ws://localhost:8888')
+
+  ws.onopen do
+    puts "Connected"
+  end
+
+  ws.onmessage do |msg, type|
+    puts "Received message: #{msg}"
+  end
+
+  ws.onclose do |code, reason|
+    puts "Disconnected with status code: #{code}"
+  end
+
+  EventMachine.next_tick do
+    ws.send "Hello Server!"
+  end
+
+end
