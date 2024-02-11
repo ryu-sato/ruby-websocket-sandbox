7894b5aa9a4c7ee968143c1d270ff524e4684ff2
Ryu Sato <ryu@weseek.co.jp>
2024-02-11 15:29:37 +0000

    ruby-clientを追加

--- Gemfile
+++ Gemfile
@@ -11,3 +11,7 @@ gem "base64", "~> 0.2.0"
 gem "websocket-eventmachine-server", "~> 1.0"
 
 gem "websocket-eventmachine-client", "~> 1.3"
+
+gem "websocket-driver", "~> 0.7.6"
+
+gem "json-rpc-objects"
--- ruby-client.rb
+++ ruby-client.rb
@@ -1,23 +1,68 @@
 require 'websocket-eventmachine-client'
+require "json-rpc-objects/v20/request"
+require "json-rpc-objects/request"
 
-EM.run do
+class WS
+    def initialize
+        @response = {}
+        Thread.new do
+            EM.run do
+                @ws = WebSocket::EventMachine::Client.connect(:uri => 'ws://localhost:8888')
 
-  ws = WebSocket::EventMachine::Client.connect(:uri => 'ws://localhost:8888')
+                @ws.onopen do
+                    puts "Connected"
+                end
 
-  ws.onopen do
-    puts "Connected"
-  end
+                @ws.onmessage do |data, type|
+                    puts "Received message: #{data}, type: #{type}"
 
-  ws.onmessage do |msg, type|
-    puts "Received message: #{msg}"
-  end
+                    parsed_data = JsonRpcObjects::Request::parse(data)
+                    parsed_data.check!
 
-  ws.onclose do |code, reason|
-    puts "Disconnected with status code: #{code}"
-  end
+                    @response[parsed_data.id] = data
+                end
 
-  EventMachine.next_tick do
-    ws.send "Hello Server!"
-  end
+                @ws.onclose do |code, reason|
+                    puts "Disconnected with status code: #{code}"
+                end
 
+                @initialized = true
+            end
+        end
+    end
+
+    def initialized?
+        @initialized
+    end
+
+    def send_sync(data)
+        parsed_data = JsonRpcObjects::Request::parse(data)
+        parsed_data.check!
+
+        @ws.send(data)
+
+        timeout = 30 # seconds
+        while !@response[parsed_data.id] || timeout <= 0
+            sleep 1
+            timeout -= 1
+        end
+        if timeout <= 0
+            puts "Timed out waiting response"
+            return nil
+        end
+
+        @response.delete(parsed_data.id)
+    end
 end
+
+ws = WS.new
+while !ws.initialized? do
+    pp 'ws is nil'
+end
+
+rpc_json_data = JsonRpcObjects::V20::Request::create(:subtract, ["1", "2"], :id => "a2b3")
+ws.send_sync(rpc_json_data.serialize)
+
+p 'start with sleep 3s...'
+sleep(3)
+p 'finish'
